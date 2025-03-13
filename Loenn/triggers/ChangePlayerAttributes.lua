b5c22b0907029f0eabceb14982f6411cccf7bc51
SnipUndercover <snipundercover@gmail.com>
2025-03-14 00:10:18 +0100

    Port the ChangePlayerAttributes trigger to Lönn

---
+++ Loenn/triggers/ChangePlayerAttributes.lua
@@ -0,0 +1,18 @@
+local trigger = {}
+
+trigger.name = "Styline/ChangePlayerAttributes"
+trigger.placements = {
+  name = "rectangle",
+  data = {
+    applyImmediately = false,
+    applyOnce = false,
+    hairColor = "",
+    hairStyle = "",
+    hairAccessory = "",
+    hairAccessoryColor = "",
+    shirtColor = "",
+    blushColor = ""
+  }
+}
+
+return trigger
